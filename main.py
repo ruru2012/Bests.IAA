c5b8b3992b1fb64473f8affdf23b31bd4f0db2fc
ruru2012 <ruansixto98@gmail.com>
2025-07-19 18:51:22 -0300

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
-from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
-import time
